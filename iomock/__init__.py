3216b2593c361e10ed1e36d039fad2bee75445cf
Martin Sivak <msivak@redhat.com>
2012-12-14 10:41:35 +0100

    Change license headers

--- iomock/__init__.py
+++ iomock/__init__.py
@@ -1,3 +1,23 @@
+# Python library for testing file operations without touching the harddrive
+#
+# Copyright (C) 2012  Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# Red Hat Author(s): Martin Sivak <msivak@redhat.com>
+#
 """This module provides methods which allow testing of IO operations
 without touching the real hardware. It is intended to be used in
 unittesting environments."""
--- iomock/disk.py
+++ iomock/disk.py
@@ -1,5 +1,6 @@
-# Copyright (C) 2010
-# Red Hat, Inc.  All rights reserved.
+# This file contains the implementation of virtual StringIO based filesystem
+#
+# Copyright (C) 2010  Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
